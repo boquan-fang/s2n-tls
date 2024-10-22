2036a245e3e1bcbef6ea9c8ddfc9595f76b29b55
Boquan Fang <bqfang@amazon.com>
2024-10-22 17:14:08 +0000

    ci: test my theory on exit the child process
    * use exit for s2n_fork_generation_number_test.c in clone

--- tests/unit/s2n_fork_generation_number_test.c
+++ tests/unit/s2n_fork_generation_number_test.c
@@ -190,11 +190,8 @@ static int s2n_unit_test_clone_child_process(void *parent_process_fgn)
     /* Verify in threads */
     EXPECT_EQUAL(s2n_unit_test_thread(return_fork_generation_number), S2N_SUCCESS);
 
-    /* Call rand cleanup to close /dev/urandom */
-    POSIX_GUARD_RESULT(s2n_rand_cleanup());
-
     /* This translates to the exit code for this child process */
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
 
 #define PROCESS_CHILD_STACK_SIZE (1024 * 1024) /* Suggested by clone() man page... */
